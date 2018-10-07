2e93fac2784edaa669aab20db6988f0d5677a5cc
sergeybandar <sergeybandar@mail.ru>
2018-10-07 14:41:28 +0300

    additional_7

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 module.exports = function solveSudoku(matrix) 
-{var p = []
- var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+{var t = []
+ var num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ var mas=new Array;
+ mas=matrix;
  for (var row = 0; row < 9; row++) {
  for (var col = 0; col < 9; col++) {
 
- if ( matrix[row][col] == 0 ) {
+ if ( mas[row][col] == 0 ) {
 
  var PositRow = Math.floor(row / 3) * 3;
  var PositCol = Math.floor(col / 3) * 3;
 
  for (var i = 0; i < 9; i++) {
- p.push(matrix[PositRow + i % 3][PositCol + Math.floor(i / 3)]);
- p.push(matrix[row][i]);
- p.push(matrix[i][col]);
+ t.push(mas[PositRow + i % 3][PositCol + Math.floor(i / 3)]);
+ t.push(mas[row][i]);
+ t.push(mas[i][col]);
  }
 
- p.filter(number => number > 0);
- var zer = numbers.filter(number => p.indexOf(number) < 0);
+ t.filter(num => num > 0);
+ var zer = num.filter(num => t.indexOf(num) < 0);
 
  for (var i = 0; i < zer.length; i++) {
- matrix[row][col] = zer[i];
- if (solveSudoku(matrix)) {
- return solveSudoku(matrix);
+ mas[row][col] = zer[i];
+ if (solveSudoku(mas)) {
+ return solveSudoku(mas);
  }
  }
 
- matrix[row][col] = 0;
- return !matrix;
+ mas[row][col] = 0;
+ return !mas;
  }
  }
  }
- return matrix;
+ return mas;
 
 }
